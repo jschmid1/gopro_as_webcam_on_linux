8cba48ac3ebb7f0fd0aace24788c5708daf56300
Joshua Schmid <jxs@posteo.de>
2021-01-24 16:49:28 +0100

    core: add an install script
    This will install a `gopro` executable to
    /usr/local/sbin/gopro
    
    Signed-off-by: Joshua Schmid <jxs@posteo.de>

---
+++ install.sh
@@ -0,0 +1,31 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+function red {
+    printf "${RED}$@${NC}\n"
+}
+
+function green {
+    printf "${GREEN}$@${NC}\n"
+}
+
+function yellow {
+    printf "${YELLOW}$@${NC}\n"
+}
+
+curl -s https://raw.githubusercontent.com/jschmid1/gopro_as_webcam_on_linux/master/gopro -o /usr/local/sbin/gopro
+
+chmod +x  /usr/local/sbin/gopro
+
+yellow "**********************"
+printf "\n\n"
+green "The GoPro install script succeeded"
+green "Run with with: "
+green "sudo gopro"
+printf "\n\n"
+yellow "**********************"
+
+
+sudo gopro
